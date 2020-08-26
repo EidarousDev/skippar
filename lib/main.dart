0c778b2c9413a9c8c8027c3bd98f0256bf437030
EidarousDev <eidarousdev@gmail.com>
2020-08-26 12:56:14 +0200

    Unable to create JsDialog without an Activity

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -11,18 +13,39 @@ void main() => runApp(MaterialApp(
       home: MyHomePage(),
     ));
 
+const String kNavigationExamplePage = '''
+<!DOCTYPE html><html>
+<head><title>Navigation Delegate Example</title></head>
+<body>
+<p>
+The navigation delegate is set to block navigation to the youtube website.
+</p>
+<ul>
+<ul><a href="https://www.youtube.com/">https://www.youtube.com/</a></ul>
+<ul><a href="https://www.google.com/">https://www.google.com/</a></ul>
+</ul>
+</body>
+</html>
+''';
+
 class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final flutterWebViewPlugin = FlutterWebviewPlugin();
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
   String selectedUrl = 'https://skippar.com';
   // ignore: prefer_collection_literals
   final Set<JavascriptChannel> jsChannels = [
     JavascriptChannel(
         name: 'Print',
+        onMessageReceived: (JavascriptMessage message) {
+          print(message.message);
+        }),
+    JavascriptChannel(
+        name: 'alert',
         onMessageReceived: (JavascriptMessage message) {
           print(message.message);
         }),
@@ -54,63 +77,125 @@ class _MyHomePageState extends State<MyHomePage> {
                     flex: 10,
                     child: Stack(
                       children: <Widget>[
-                        WebviewScaffold(
-                          url: selectedUrl,
-                          javascriptChannels: jsChannels,
-                          mediaPlaybackRequiresUserGesture: false,
-                          withZoom: true,
-                          withLocalStorage: true,
-                          hidden: true,
-                          initialChild: Container(
-                            color: Colors.deepPurple,
-                            child: const Center(
-                              child: CircularProgressIndicator(
-                                backgroundColor: Colors.cyanAccent,
-                                strokeWidth: 10,
-                              ),
-                            ),
-                          ),
-                          bottomNavigationBar: BottomAppBar(
-                            child: Row(
-                              children: <Widget>[
-                                IconButton(
-                                  icon: const Icon(Icons.arrow_back_ios),
-                                  onPressed: () {
-                                    flutterWebViewPlugin.goBack();
-                                  },
-                                ),
-                                IconButton(
-                                  icon: const Icon(Icons.arrow_forward_ios),
-                                  onPressed: () {
-                                    flutterWebViewPlugin.goForward();
-                                  },
-                                ),
-                                IconButton(
-                                  icon: const Icon(Icons.autorenew),
-                                  onPressed: () {
-                                    flutterWebViewPlugin.reload();
-                                  },
-                                ),
-                              ],
-                            ),
-                          ),
+                        WebView(
+                          initialUrl: 'https://skippar.com/',
+                          javascriptMode: JavascriptMode.unrestricted,
+                          gestureNavigationEnabled: true,
+                          onWebViewCreated:
+                              (WebViewController webViewController) {
+                            _controller.complete(webViewController);
+                          },
+
+                          // TODO(iskakaushik): Remove this when collection literals makes it to stable.
+                          // ignore: prefer_collection_literals
+                          javascriptChannels: <JavascriptChannel>[
+                            _toasterJavascriptChannel(context),
+                          ].toSet(),
+                          navigationDelegate: (NavigationRequest request) {
+                            if (request.url
+                                .startsWith('https://www.youtube.com/')) {
+                              print('blocking navigation to $request}');
+                              return NavigationDecision.prevent;
+                            }
+                            print('allowing navigation to $request');
+                            return NavigationDecision.navigate;
+                          },
                         ),
                         (showLoading)
                             ? Center(
                                 child: CircularProgressIndicator(),
                               )
-                            : Center()
+                            : Center(),
                       ],
                     )),
               ],
             ),
           ),
         ),
+        bottomNavigationBar: NavigationControls(_controller.future),
       ),
     );
   }
 
+  @override
+  void initState() {
+    super.initState();
+  }
+
   Future<bool> _onBackPressed() {
-    return flutterWebViewPlugin.goBack();
+    //return _controller.goBack();
+  }
+
+  JavascriptChannel _toasterJavascriptChannel(BuildContext context) {
+    return JavascriptChannel(
+        name: 'Toaster',
+        onMessageReceived: (JavascriptMessage message) {
+          Scaffold.of(context).showSnackBar(
+            SnackBar(content: Text(message.message)),
+          );
+        });
+  }
+}
+
+class NavigationControls extends StatelessWidget {
+  const NavigationControls(this._webViewControllerFuture)
+      : assert(_webViewControllerFuture != null);
+
+  final Future<WebViewController> _webViewControllerFuture;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<WebViewController>(
+      future: _webViewControllerFuture,
+      builder:
+          (BuildContext context, AsyncSnapshot<WebViewController> snapshot) {
+        final bool webViewReady =
+            snapshot.connectionState == ConnectionState.done;
+        final WebViewController controller = snapshot.data;
+        return Row(
+          children: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.arrow_back_ios),
+              onPressed: !webViewReady
+                  ? null
+                  : () async {
+                      if (await controller.canGoBack()) {
+                        await controller.goBack();
+                      } else {
+                        Scaffold.of(context).showSnackBar(
+                          const SnackBar(content: Text("No back history item")),
+                        );
+                        return;
+                      }
+                    },
+            ),
+            IconButton(
+              icon: const Icon(Icons.arrow_forward_ios),
+              onPressed: !webViewReady
+                  ? null
+                  : () async {
+                      if (await controller.canGoForward()) {
+                        await controller.goForward();
+                      } else {
+                        Scaffold.of(context).showSnackBar(
+                          const SnackBar(
+                              content: Text("No forward history item")),
+                        );
+                        return;
+                      }
+                    },
+            ),
+            IconButton(
+              icon: const Icon(Icons.replay),
+              onPressed: !webViewReady
+                  ? null
+                  : () {
+                      controller.reload();
+                    },
+            ),
+          ],
+        );
+      },
+    );
   }
 }
